ddf561c8d2ef1da9b6e23f992678c6295990d6bd
igoji <ignacioantonioo@gmail.com>
2024-05-21 14:04:11 -0400

    feat: added Specialty entity and specialty attribute to Doctor Entity

---
+++ src/main/java/com/backend/mediConnect/entity/Specialty.java
@@ -0,0 +1,48 @@
+package com.backend.mediConnect.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "SPECIALTIES")
+public class Specialty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    public Specialty(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
