617fc9eafc27d86221b760648a68d823353d8fa3
nrealr <nrealr@gmail.com>
2024-05-29 16:53:59 -0400

    solving error

--- src/main/java/com/backend/mediConnect/config/SecurityConfig.java
+++ src/main/java/com/backend/mediConnect/config/SecurityConfig.java
@@ -18,6 +18,11 @@ import com.backend.mediConnect.config.jwt.JwtAccessDeniedError;
 import com.backend.mediConnect.config.jwt.JwtAuthenticationError;
 import com.backend.mediConnect.config.jwt.JwtRequestFilter;
 import com.backend.mediConnect.service.impl.UserDetailService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -45,6 +50,19 @@ public class SecurityConfig {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); 
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.
                 authorizeHttpRequests(authConfig -> {
@@ -59,7 +77,7 @@ public class SecurityConfig {
             authConfig.anyRequest().authenticated();
         })
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
                 .exceptionHandling(exceptionHandling -> {
                     exceptionHandling
