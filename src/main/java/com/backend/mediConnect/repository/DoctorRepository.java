47fe51535cbbb1389e56e4168f1faf0f6940d207
nrealr <nrealr@gmail.com>
2024-06-28 16:01:35 -0400

    fix search bug

--- src/main/java/com/backend/mediConnect/repository/DoctorRepository.java
+++ src/main/java/com/backend/mediConnect/repository/DoctorRepository.java
@@ -18,10 +18,28 @@ public interface DoctorRepository extends JpaRepository<Doctor, Long> {
             "LEFT JOIN FETCH d.specialty s " +
             "LEFT JOIN FETCH d.features f " +
             "LEFT JOIN d.location l " +
-            "WHERE (LOWER(d.name) LIKE LOWER(CONCAT('%', :query, '%')) " +
-            "OR LOWER(d.lastname) LIKE LOWER(CONCAT('%', :query, '%')) " +
-            "OR LOWER(s.name) LIKE LOWER(CONCAT('%', :query, '%')) " +
-            "OR LOWER(f.name) LIKE LOWER(CONCAT('%', :query, '%'))) " +
-            "AND LOWER(l.name) LIKE LOWER(CONCAT('%', :location, '%'))")
-    List<Doctor> searchDoctors(@Param("query") String query, @Param("location") String location);
+            "WHERE (" +
+            "LOWER(d.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(d.lastname) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(s.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(f.name) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
+            ") AND LOWER(l.name) LIKE LOWER(CONCAT('%', :location, '%'))")
+    List<Doctor> searchDoctors(@Param("keyword") String keyword, @Param("location") String location);
+
+    // Nuevo método para manejar múltiples palabras clave
+    @Query("SELECT DISTINCT d FROM Doctor d " +
+            "LEFT JOIN FETCH d.specialty s " +
+            "LEFT JOIN FETCH d.features f " +
+            "LEFT JOIN d.location l " +
+            "WHERE (" +
+            "LOWER(d.name) LIKE LOWER(CONCAT('%', :keyword1, '%')) OR " +
+            "LOWER(d.lastname) LIKE LOWER(CONCAT('%', :keyword1, '%')) OR " +
+            "LOWER(s.name) LIKE LOWER(CONCAT('%', :keyword1, '%')) OR " +
+            "LOWER(f.name) LIKE LOWER(CONCAT('%', :keyword1, '%')) OR " +
+            "LOWER(d.name) LIKE LOWER(CONCAT('%', :keyword2, '%')) OR " +
+            "LOWER(d.lastname) LIKE LOWER(CONCAT('%', :keyword2, '%')) OR " +
+            "LOWER(s.name) LIKE LOWER(CONCAT('%', :keyword2, '%')) OR " +
+            "LOWER(f.name) LIKE LOWER(CONCAT('%', :keyword2, '%'))" +
+            ") AND LOWER(l.name) LIKE LOWER(CONCAT('%', :location, '%'))")
+    List<Doctor> searchDoctorsWithMultipleKeywords(@Param("keyword1") String keyword1, @Param("keyword2") String keyword2, @Param("location") String location);
 }
